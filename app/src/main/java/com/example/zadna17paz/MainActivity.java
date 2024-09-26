ce9b278427524181c0720d6ce8dd8e99a2962e0e
Vergin <mrtemnmie@gmail.com>
2024-09-26 14:23:44 +0200

    first commit of work

--- app/src/main/java/com/example/zadna17paz/MainActivity.java
+++ app/src/main/java/com/example/zadna17paz/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.zadna17paz;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+private TextView textViewCount;
+private Button buttonIncrementCounter;
+private EditText userText;
+private CheckBox checkBox;
+private Switch changeBGColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        textViewCount = findViewById(R.id.textViewCount);
+        buttonIncrementCounter = findViewById(R.id.buttonIncrement);
+        userText = findViewById(R.id.userText);
+        checkBox = findViewById(R.id.checkBox);
+        changeBGColor = findViewById(R.id.changeBGColor);
     }
 }
