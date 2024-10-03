dc3101e542038544e9971f2228901fdaa447c6ff
Vergin <mrtemnmie@gmail.com>
2024-10-03 13:30:26 +0200

    more work

--- app/src/main/java/com/example/zadna17paz/MainActivity.java
+++ app/src/main/java/com/example/zadna17paz/MainActivity.java
@@ -1,30 +1,61 @@
 package com.example.zadna17paz;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-private TextView textViewCount;
-private Button buttonIncrementCounter;
-private EditText userText;
-private CheckBox checkBox;
-private Switch changeBGColor;
+    private static final String KEY_COUNT = "count";
+    private TextView textViewCount;
+    private Button buttonIncrementCounter;
+    private EditText userText;
+    private CheckBox checkBox;
+    private Switch changeBGColor;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         textViewCount = findViewById(R.id.textViewCount);
         buttonIncrementCounter = findViewById(R.id.buttonIncrement);
         userText = findViewById(R.id.userText);
         checkBox = findViewById(R.id.checkBox);
         changeBGColor = findViewById(R.id.changeBGColor);
+
+        if(savedInstanceState != null){
+            count = savedInstanceState.getInt(KEY_COUNT);
+        }
+        updateCountText();
+
+        buttonIncrementCounter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                updateCountText();
+            }
+        });
+
     }
+    private void updateCountText(){
+        textViewCount.setText("licznik: " + count);
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_COUNT, count);
+
+    }
+    private void bgColor(){
+        if(changeBGColor.isActivated()){
+
+        }
+    }
+
 }
